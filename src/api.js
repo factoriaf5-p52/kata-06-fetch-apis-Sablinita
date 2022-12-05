d0ea2c7928dd57d4723886ce0fb25c74b38d2255
Sablinita <sablinahare@gmail.com>
2022-12-05 10:37:53 +0100

    given a different solution by Raul

--- src/api.js
+++ src/api.js
@@ -5,16 +5,40 @@
 
 //api --> https://uselessfacts.jsph.pl/random.json?language=en
 
-document.querySelector("button").addEventListener("click", ()=> {
+/*REFACTORING
+1. Dejar el código más legible
+2. Menos cantidad de líneas de código
+3. Una función una responsabilidad
+4. No mezclemos capas de abstracción
+*/
 
-let myParagraph = document.querySelector("p")
 
-fetch('https://uselessfacts.jsph.pl/random.json?language=en')
-   .then(result => result.json())
-   .then(data => {
-       myParagraph.textContent = data.text
-   })
-   .catch(error => console.log(error))
+const myParagraph = document.querySelector('p');
+const button = document.querySelector('button');
 
-  })
+// async/await
+//Función qu etrae datos de la Api
+const getApiData = async () =>{
+
+  const result = await fetch('https://uselessfacts.jsph.pl/random.json?language=en');
+    const data = await result.json();
+    return data.text;
+  }
+
+  //Función que pinta los datos en el párrafo
+  const printData = (text) =>{
+    myParagraph.innerText = text
+  }
+ 
+//Función que se ejecuta con el click del botón
+const buttonClick = async () => {
+    const text = await getApiData();
+    printData(text);
+}
+
+button.addEventListener("click", buttonClick);
+
+
+
+
 
